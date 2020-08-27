932b105cad6cc0deacf6a41f40739b0bdd415d9f
guistetter <guilherme_stetter@hotmail.com>
2020-08-27 13:49:42 -0300

    criando session de usuario

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const session = require('express-session')
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise 
+require('dotenv/config')
 
 app.use(express.static('public'))
+app.use(bodyParser.json({extended: true}))
 app.use(bodyParser.urlencoded())
 app.set('view engine', 'ejs')
+app.use(session({
+  secret: 'meuSegredo',
+  name:"sessionID",
+  cookie: {
+    maxAge: 10*60*1000
+  }
+}))
 
 app.get('/',(req, res) => {
   res.render('home')
 })
+
 app.post('/', (req, res) => {
   console.log(req.body.name)
-  res.send('ok')
+  req.session.user = {
+    name: req.body.name
+  }
+  res.redirect('/room')
+})
+
+app.get('/room', (req, res) => {
+  res.send(req.session)
 })
 
 //servidor só inicia apos o mongo iniciar
