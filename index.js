aeef11b29d96a59b81d0534313d38eb54677db96
guistetter <guilherme_stetter@hotmail.com>
2020-08-27 13:29:21 -0300

    criando rota post para pegar nome do formulario de 'login'

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const app = express()
-
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise 
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded())
 app.set('view engine', 'ejs')
 
 app.get('/',(req, res) => {
   res.render('home')
+})
+app.post('/', (req, res) => {
+  console.log(req.body.name)
+  res.send('ok')
 })
 
 //servidor só inicia apos o mongo iniciar
